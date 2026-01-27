2620b4d08c42052a038e7a8df88a7698c1583a2d
James Fryar <jfryar07@gmail.com>
2026-01-27 16:04:41 -0500

    Fix step function so code compiles

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,7 +1,13 @@
-#include <SDL2/SDL.h>
+//#include <SDL2/SDL.h>
 #include <iostream>
+#include "cpu.h"
 
 int main(){
+    
+    Bus bus;
+    Bus *b = &bus;
+    registers regs;
+    registers *r = &regs;
 
     return 0;
 }
--- src/cpu.h
+++ src/cpu.h
@@ -163,35 +163,43 @@ class CPU
             switch(opCode){
                 
                 case(0x0):
-
+                {
                     //On memory map row 0x column x0 NOP increment PC by 1 byte
                     r->PC += 1;
 
                     break;
+                }
 
                 case(0x01):
+                {
                     // LD BC, n16 3 12 ----
                     //On memory map row 0x column x1 fetch 16 bit int and assign it to bc register
                     r->bc = fetch_16();
                     r->PC += 3;
                     break;
+                }
 
                 case(0x02):
+                {
                     // LD [BC], A 1 8 ----
                     //On memory map row 0x column x2 write contents of A to address stored in bc registry
                     b->write_8(r->bc,r->a);
                     r->PC += 1;
 
-                    break;
-
+                    break; 
+                }
+                    
                 case(0x03):
+                {
                     // INC BC 1 8 ----
                     //On memory map row 0x column x3 increment the BC register by one bit 
                     r->bc += 1;
                     r->PC += 1;
                     break;
+                }
 
                 case(0x04):
+                {
                     // INC B 1 4 Z 0 H -
                     //On memory map row 0x column x4 increment B register set z n and h flags accordingly
                     r->b = inc8(r->b);
@@ -199,15 +207,20 @@ class CPU
 
                     break;
 
+                }
+                    
                 case(0x05):
+                {
                     // DEC B 1 4 Z 1 H -
                     //On memory map row 0x column x5 decrement B register set z n and h flags accordingly
                     r->b = dec8(r->b);
                     r->PC += 1;
 
                     break;
+                }    
                 
                 case(0x06):
+                {
                     //LD B n8 2 8 ----
                     //On memory map row 0x column x6 fetch 8 bit int and assign to b register;
 
@@ -215,8 +228,10 @@ class CPU
                     r->PC += 2;
 
                     break;
-
+                }
+                    
                 case(0x07):
+                {
                     // RCLA 1 4 0 0 0 C
                     //On memory map row 0x column x7 rotate accumulator bits to left bit 7 wraps around to bit 0
 
@@ -237,15 +252,20 @@ class CPU
                     r->PC += 1;
 
                     break;
+                }
+
 
                 case(0x08):
+                {
                     // LD [a16], SP 3 20 ----
                     //On memory map row 0x column x8 write stack pointer to address after opcode
                     b->write_16(r->PC + 1, r->SP);
                     r->PC += 3;
                     break;
-
+                }
+                    
                 case(0x09):
+                {
                     // ADD HL, BC 1 8 - 0 H C
                     //On memory map row 0x column x9 add value in BC register to HL register
                     uint16_t val1 = r->hl;
@@ -261,44 +281,56 @@ class CPU
                     //See if result if greater then 16 bits
                     setC(result > 0xFFFF);
                     break;
-                
+                }
+                    
                 case(0x0A):
+                {
                     // LD A, [BC] 1 8 ----
                     //On memory map row 0x column xA load the the value stored in at the memory address in the BC register to register A
                     r->a = b->read_8(r->bc);
                     r->PC += 1;
                     break;
-                
+                }
+
                 case(0x0B):
+                {
                     // DEC BC 1 8 ----
                     //On memory map row 0x column xB decrement BC register
                     r->bc -= 1;
                     r->PC += 1;
                     break;
+                }
 
                 case(0x0C):
+                {
                     // INC C 1 4 Z 0 H -
                     //On memory map row 0x column xC increment C register and raise z n and h flags based on result
                     r->c = inc8(r->c);
                     r->PC += 1;
 
                     break;
+                }
 
                 case(0x0D):
+                {
                     // DEC C 1 4 Z 1 H -
                     //One memory map row 0x column xD decrement register C and raise z n and h flags based on result
                     r->c = dec8(r->c);
                     r->PC += 1;
                     break;
+                }
 
                 case(0x0E):
+                {
                     //LD C n8 2 8 ----
                     //On memory map row 0x column xE fetch 8 bit int and assign to C register;
                     r->c = fetch_8();
                     r->PC += 2;
                     break;
-
+                }
+                    
                 case(0x0F):
+                {
                     //RRCA 1 4 0 0 0 C
                     uint16_t bit0 = r->a;
                     r->a = r->a >> 1;
@@ -309,6 +341,8 @@ class CPU
                     setN(false);
                     setH(false);
                     setC(bit0);
+                }
+                    
             }
 
             return 0;
